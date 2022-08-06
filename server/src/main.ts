6b5d77e580ac9a5c8f539bc48665a49e1dbd679e
padosum <makalu171@gmail.com>
2022-08-06 14:56:21 +0900

    feat ✨ swagger 적용

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { setupSwagger } from './util/swagger';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
   app.enableCors();
+
+  setupSwagger(app);
   await app.listen(3000);
 }
 bootstrap();
---
+++ server/src/util/swagger.ts
@@ -0,0 +1,18 @@
+import { INestApplication } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+/**
+ * Swagger 세팅
+ *
+ * @param {INestApplication} app
+ */
+export function setupSwagger(app: INestApplication): void {
+  const options = new DocumentBuilder()
+    .setTitle('우아한 키오스크 API Docs')
+    .setDescription('📝 우아한 키오스크 API 문서입니다.')
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document);
+}
