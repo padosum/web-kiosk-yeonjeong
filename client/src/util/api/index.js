85af804969d40fc724fe01a7a0510f92168906a8
padosum <makalu171@gmail.com>
2022-08-08 02:09:43 +0900

    feat ✨ fetch 함수 예외 처리 추가

--- client/src/util/api/index.js
+++ client/src/util/api/index.js
@@ -1,17 +1,19 @@
 const fetcher = (path, method, body) =>
-  new Promise((resolve) => {
+  new Promise((resolve, reject) => {
     fetch(path, {
       method,
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(body),
-    }).then((res) => {
-      if (!res.ok) {
-        throw new Error('요청 실패')
-      }
-      resolve(res.json())
     })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error('요청 실패')
+        }
+        resolve(res.json())
+      })
+      .catch((err) => reject(err))
   })
 
 const API = {
