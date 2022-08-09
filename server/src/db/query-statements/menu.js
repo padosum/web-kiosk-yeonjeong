aefd2acd2bfbbac721a4d3a112da74503b2c2597
padosum <makalu171@gmail.com>
2022-08-09 13:13:50 +0900

    feat ✨ 주문 관련 query문 파일 분리

--- server/src/db/query-statements/menu.js
+++ server/src/db/query-statements/menu.js
@@ -35,48 +35,4 @@ const getAllMenu = () => `
    ORDER BY salesCnt DESC;
     `;
 
-const getOrderNum = () => `
-   SELECT (SELECT COUNT(*) + 1 
-             FROM ORDERS 
-            WHERE dateString = '${getTodayDateString()}' 
-              AND id < O.id) AS orderNum
-     FROM ORDERS O
-    WHERE O.id = ?
-`;
-
-const insertOrders = () => `
-    INSERT INTO ORDERS (
-      paymentId
-    , paymentAmount
-    , totalAmount
-    , dateString
-    ) 
-    VALUES 
-    (
-      ?
-    , ?
-    , ?
-    , '${getTodayDateString()}'
-    );
-    `;
-
-const insertOrdersHasMenu = () => `
-   INSERT INTO ORDERS_HAS_MENU (
-      ordersId
-    , menuId
-    , menuOptionId
-    ) 
-    VALUES 
-    (
-      ?
-    , ?
-    , ?
-    )
-    `;
-export {
-  getAllMenu,
-  getAllCategory,
-  getOrderNum,
-  insertOrders,
-  insertOrdersHasMenu,
-};
+export { getAllMenu, getAllCategory };
