c06b3044ee286b5bc374df997a8ce453a90a5148
padosum <makalu171@gmail.com>
2022-08-10 00:05:23 +0900

    refactor ♻️ CartLayout 컴포넌트 분리

--- client/src/components/pages/Main/index.js
+++ client/src/components/pages/Main/index.js
@@ -1,17 +1,15 @@
 import React, { useEffect, useState } from 'react'
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
 
 import API from '../../../util/api'
 import Tabs from '../../layout/Tabs'
-import Container from '../../common/Container'
-import Badge from '../../common/Badge'
 import Button from '../../common/Button'
 import Counter from '../../common/Counter'
 import Payment from '../../layout/Payment'
 import LoadingIndicator from '../../common/LoadingIndicator'
 import Receipt from '../../layout/Receipt'
 import Input from '../../common/Input'
-import ItemImageContainer from '../../common/ItemImageContainer'
+import CartLayout from '../../layout/CartLayout'
 
 const ItemsLayout = styled.section`
   display: flex;
@@ -26,72 +24,7 @@ const CashLayout = styled.section`
   justify-content: space-around;
   padding-top: 1rem;
 `
-const CartLayout = styled.section`
-  display: flex;
-  grid-area: cart;
-  background-color: #484848;
-  border-radius: 10px;
-  border: 4px solid #353535;
-  overflow: scroll;
-  padding: 1rem;
-  box-shadow: inset -4px -4px 0px 0px #353535;
-  ${(props) =>
-    props.step === 'cash' &&
-    css`
-      opacity: 0.6;
-    `}
-`
 
-const ButtonWrapper = styled.div`
-  display: flex;
-  justify-content: flex-end;
-`
-const SelectItemWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  padding: 1rem;
-  flex: 0 0 150px;
-  margin: 0 0.5rem;
-  background-color: #fff;
-  border-radius: 10px;
-  border: 4px solid #353535;
-
-  @keyframes drop {
-    0% {
-      transform: translateY(-30px) scaleY(0.9);
-      opacity: 0;
-    }
-    5% {
-      opacity: 0.7;
-    }
-    50% {
-      transform: translateY(0px) scaleY(1);
-      opacity: 1;
-    }
-    65% {
-      transform: translateY(-17px) scaleY(0.9);
-      opacity: 1;
-    }
-    75% {
-      transform: translateY(-22px) scaleY(0.9);
-      opacity: 1;
-    }
-    100% {
-      transform: translateY(0px) scaleY(1);
-      opacity: 1;
-    }
-  }
-
-  opacity: 0;
-  animation: drop 0.4s linear forwards 0.4s;
-`
-
-const OptionWrapper = styled.span`
-  text-align: center;
-  margin: 0.25rem;
-  font-size: 1.25rem;
-`
 const PaymentLayout = styled.section`
   display: flex;
   flex-direction: column;
@@ -278,39 +211,11 @@ function Main() {
               현금 결제하기
             </Button>
           </CashLayout>
-          <CartLayout step={step}>
-            {selectMenu.length > 0 &&
-              selectMenu.map((item) => {
-                return (
-                  <SelectItemWrapper key={item.menuId}>
-                    <ButtonWrapper>
-                      {step !== 'cash' && (
-                        <button
-                          onClick={() =>
-                            handleRemoveMenu(item.menuId, item.optionId)
-                          }
-                        >
-                          X
-                        </button>
-                      )}
-                    </ButtonWrapper>
-                    <Container title={item.title}>
-                      <ItemImageContainer
-                        src={`images/${item.categoryId}.png`}
-                        alt="product"
-                      ></ItemImageContainer>
-                      <Badge variant="normal" icon={false}>
-                        {item.price.toLocaleString()}
-                      </Badge>
-                    </Container>
-                    <OptionWrapper>
-                      <p>{item.optionTitle.split(',').join('/')}</p>
-                      <p>수량: {item.quantity}개</p>
-                    </OptionWrapper>
-                  </SelectItemWrapper>
-                )
-              })}
-          </CartLayout>
+          <CartLayout
+            selectMenu={selectMenu}
+            step={step}
+            handleRemoveMenu={handleRemoveMenu}
+          ></CartLayout>
           <PaymentLayout>
             {selectMenu.length > 0 && step === 'main' && (
               <>
