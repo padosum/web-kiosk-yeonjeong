d583f5db8735fd9a14d1ddc52ba248a5598ecc2d
padosum <makalu171@gmail.com>
2022-08-09 13:11:52 +0900

    feat ✨ 주문관련 query문 추가

--- server/src/db/query-statements/menu.js
+++ server/src/db/query-statements/menu.js
@@ -35,4 +35,48 @@ const getAllMenu = () => `
    ORDER BY salesCnt DESC;
     `;
 
-export { getAllMenu, getAllCategory };
+const getOrderNum = () => `
+   SELECT (SELECT COUNT(*) + 1 
+             FROM ORDERS 
+            WHERE dateString = '${getTodayDateString()}' 
+              AND id < O.id) AS orderNum
+     FROM ORDERS O
+    WHERE O.id = ?
+`;
+
+const insertOrders = () => `
+    INSERT INTO ORDERS (
+      paymentId
+    , paymentAmount
+    , totalAmount
+    , dateString
+    ) 
+    VALUES 
+    (
+      ?
+    , ?
+    , ?
+    , '${getTodayDateString()}'
+    );
+    `;
+
+const insertOrdersHasMenu = () => `
+   INSERT INTO ORDERS_HAS_MENU (
+      ordersId
+    , menuId
+    , menuOptionId
+    ) 
+    VALUES 
+    (
+      ?
+    , ?
+    , ?
+    )
+    `;
+export {
+  getAllMenu,
+  getAllCategory,
+  getOrderNum,
+  insertOrders,
+  insertOrdersHasMenu,
+};
