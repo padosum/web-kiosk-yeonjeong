1e1bb0bb04828879532d3c7102b243f0414a27fc
padosum <makalu171@gmail.com>
2022-08-11 15:29:36 +0900

    feat ✨ 주문 내역 추가 API 오류시 alert 보여주기

--- client/src/components/layout/CashLayout.js
+++ client/src/components/layout/CashLayout.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import API from '../../util/api'
+import AlertModal from '../common/AlertModal'
 import Button from '../common/Button'
 import Input from '../common/Input'
 
@@ -22,62 +23,77 @@ const CashLayout = ({
   paymentAmount,
   setPaymentAmount,
 }) => {
+  const [error, setError] = useState(false)
   const totalAmount = selectMenu.reduce((acc, curr) => acc + curr.price, 0)
   const orderMenu = async () => {
-    const orderNum = await API.post(`${BASE_URL}/api/orders`, {
-      paymentId: 1,
-      paymentAmount,
-      totalAmount,
-      menu: [...selectMenu],
-    })
+    try {
+      const orderNum = await API.post(`${BASE_URL}/api/orders`, {
+        paymentId: 1,
+        paymentAmount,
+        totalAmount,
+        menu: [...selectMenu],
+      })
 
-    setPayment((prevPayment) => {
-      return {
-        ...prevPayment,
-        orderNum,
-      }
-    })
-    setStep('receipt')
+      setPayment((prevPayment) => {
+        return {
+          ...prevPayment,
+          orderNum,
+        }
+      })
+      setStep('receipt')
+    } catch (err) {
+      setError(true)
+    }
   }
 
   const amountList = [100, 500, 1000, 10000]
   return (
-    <CashLayoutStyle>
-      <Input
-        title="주문금액"
-        value={totalAmount.toLocaleString()}
-        color="white"
-      ></Input>
-      <Input
-        title="투입금액"
-        value={paymentAmount.toLocaleString()}
-        color="white"
-      ></Input>
-      {amountList.map((amount) => {
-        return (
-          <Button
-            key={amount}
-            size="lg"
-            variant="normal"
-            data-amount={amount}
-            disabled={step !== 'cash'}
-            onClick={() =>
-              setPaymentAmount((prevPaymentAmout) => prevPaymentAmout + amount)
-            }
-          >
-            {amount.toLocaleString() + '원'}
-          </Button>
-        )
-      })}
-      <Button
-        size="lg"
-        variant="warning"
-        disabled={step !== 'cash' || totalAmount > paymentAmount}
-        onClick={() => orderMenu()}
-      >
-        현금 결제하기
-      </Button>
-    </CashLayoutStyle>
+    <>
+      {error && (
+        <AlertModal
+          message="결제 중 오류가 발생했습니다."
+          onClose={() => setError(false)}
+        ></AlertModal>
+      )}
+      <CashLayoutStyle>
+        <Input
+          title="주문금액"
+          value={totalAmount.toLocaleString()}
+          color="white"
+        ></Input>
+        <Input
+          title="투입금액"
+          value={paymentAmount.toLocaleString()}
+          color="white"
+        ></Input>
+        {amountList.map((amount) => {
+          return (
+            <Button
+              key={amount}
+              size="lg"
+              variant="normal"
+              data-amount={amount}
+              disabled={step !== 'cash'}
+              onClick={() =>
+                setPaymentAmount(
+                  (prevPaymentAmout) => prevPaymentAmout + amount
+                )
+              }
+            >
+              {amount.toLocaleString() + '원'}
+            </Button>
+          )
+        })}
+        <Button
+          size="lg"
+          variant="warning"
+          disabled={step !== 'cash' || totalAmount > paymentAmount}
+          onClick={() => orderMenu()}
+        >
+          현금 결제하기
+        </Button>
+      </CashLayoutStyle>
+    </>
   )
 }
 
