e148d00c3c4444e6ee91398a42396b01cd0bd7aa
padosum <makalu171@gmail.com>
2022-12-01 21:55:35 +0900

    ✨ server에서 빌드된 static 파일 불러오기

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,9 +4,14 @@ import { DbModule } from './db/db.module';
 import { MenuModule } from './menu/menu.module';
 import { OrdersModule } from './orders/orders.module';
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', 'client/build'),
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
